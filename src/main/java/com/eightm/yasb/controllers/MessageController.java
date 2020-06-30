8276a0f396d0b3babdc2a413eeea282b2bdb5bf8
Viktor Gukov <zchokobo@gmail.com>
2020-06-30 11:45:13 +0300

    Added post method for send messages and support for telegram token properties

---
+++ src/main/java/com/eightm/yasb/controllers/MessageController.java
@@ -0,0 +1,37 @@
+package com.eightm.yasb.controllers;
+
+import com.eightm.yasb.Message;
+import com.eightm.yasb.Task;
+import io.micronaut.context.annotation.Value;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.HttpStatus;
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Post;
+import io.micronaut.http.client.RxHttpClient;
+import io.micronaut.http.client.annotation.Client;
+import io.reactivex.Maybe;
+
+import javax.inject.Inject;
+
+import static io.micronaut.http.HttpRequest.POST;
+
+@Controller("/messages")
+public class MessageController {
+    @Value("${telegram.token}")
+    private String token;
+
+    @Client("https://api.telegram.org")
+    @Inject
+    RxHttpClient httpClient;
+
+    @Post(consumes = MediaType.APPLICATION_JSON)
+    public Maybe<HttpStatus> sayHello(Task task) {
+
+        Message message = new Message(task.getRecipientId(), task.getAuthor() + "\n" + task.getText());
+        return httpClient.exchange(
+                POST(token + "/sendMessage", message),
+                Message.class
+        ).firstElement().map(HttpResponse::getStatus);
+    }
+}
