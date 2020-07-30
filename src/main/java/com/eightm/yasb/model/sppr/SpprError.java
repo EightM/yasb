13373790bc3dfce3d93a3129354171e154fd59c7
Viktor Gukov <zchokobo@gmail.com>
2020-07-30 14:29:37 +0300

    new fields for error entity

--- src/main/java/com/eightm/yasb/model/sppr/SpprError.java
+++ src/main/java/com/eightm/yasb/model/sppr/SpprError.java
@@ -7,6 +7,13 @@ public class SpprError implements SpprEntity {
     @JsonProperty("recipient_id")
     private String recipientId;
     private String description;
+    private String name;
+    private String status;
+    private String priority;
+    private String executor;
+    private String comment;
+    private String code;
+
 
     public String getDescription() {
         return description;
@@ -24,6 +31,58 @@ public class SpprError implements SpprEntity {
     @Override
     public String getRecipientId() {
         return recipientId;
+    }
+
+    public void setRecipientId(String recipientId) {
+        this.recipientId = recipientId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public String getExecutor() {
+        return executor;
+    }
+
+    public void setExecutor(String executor) {
+        this.executor = executor;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
     }
 }
 
